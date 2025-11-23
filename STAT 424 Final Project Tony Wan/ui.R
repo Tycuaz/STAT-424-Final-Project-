683ff133bb9a9df9e7da598d406a74a02e7079d3
Tycuaz <159326149+Tycuaz@users.noreply.github.com>
2025-11-24 00:51:38 +0800

    Update ui.R

--- STAT 424 Final Project Tony Wan/ui.R
+++ STAT 424 Final Project Tony Wan/ui.R
@@ -4,34 +4,43 @@ shinyUI(
   navbarPage(
     "Climate Dashboard",
     
+
     tabPanel(
       "Energy & Emissions",
-      sidebarLayout(
-        sidebarPanel(
-          uiOutput("year_selector"),
-          checkboxInput("show_legend1", "Show Legend", TRUE),
-          helpText("Explore whether higher renewable energy use corresponds to lower CO₂ emissions.")
-        ),
-        mainPanel(
-          plotly::plotlyOutput("plot1", height = 500)
+      
+      plotly::plotlyOutput("plot1", height = 500),
+      
+      fluidRow(
+        column(
+          width = 12,
+          div(style = "margin-top: 20px;",
+              uiOutput("year_selector"),
+              checkboxInput("show_legend1", "Show Legend", TRUE),
+              helpText("Explore whether higher renewable energy use corresponds to lower CO₂ emissions.")
+          )
         )
       )
     ),
     
+
     tabPanel(
       "Temperature & Extreme Weather",
-      sidebarLayout(
-        sidebarPanel(
-          uiOutput("country_selector"),
-          checkboxInput("show_points2", "Show Data Points", TRUE),
-          checkboxInput("show_legend2", "Show Legend", TRUE),
-          helpText("Up to 5 countries can be selected.")
-        ),
-        mainPanel(
-          plotly::plotlyOutput("plot2", height = 500)
+      
+      plotly::plotlyOutput("plot2", height = 500),
+
+      fluidRow(
+        column(
+          width = 12,
+          div(style = "margin-top: 20px;",
+              uiOutput("country_selector"),
+              checkboxInput("show_points2", "Show Data Points", TRUE),
+              checkboxInput("show_legend2", "Show Legend", TRUE),
+              helpText("Up to 5 countries can be selected.")
+          )
         )
       )
     )
   )
 )
+
 
